2a5602b95682806a8b6b73dde6a6d376f324bf56
MM <man8d9@gmail.com>
2026-06-03 08:39:02 +0530

    Add function to double items of a list

---
+++ Part 5/References/Items_multiplied_by_two.py
@@ -0,0 +1,10 @@
+def double_items(numbers : list):
+   new_list = []
+   for i in numbers:
+    new_list.append(i * 2)
+   return new_list   
+
+numbers = [2, 4, 5, 3, 11, -4]
+numbers_doubled = double_items(numbers)
+print("original:", numbers)
+print("doubled:", numbers_doubled)
