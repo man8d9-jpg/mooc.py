1eebe3d2ee13ef72680e893d1d4725cf3a0aed60
MM <man8d9@gmail.com>
2026-06-12 08:58:00 +0530

    Store factorials in dictionary

---
+++ Part 5/Dictionary/Factorials.py
@@ -0,0 +1,16 @@
+def factorials(n: int):
+    fac_dictionary = {}
+
+    for i in range(1, n+1):
+        fact = 1
+        for j in range(1, i+1):
+            fact *= j
+        fac_dictionary[i] = fact
+    return fac_dictionary
+
+k = factorials(5)
+print(k[1])
+print(k[3])
+print(k[5])
+
+
